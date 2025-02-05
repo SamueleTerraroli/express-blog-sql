7436bdf95399c9d8833f96a587a4f2b350de78ee
SamueleTerraroli <terraroli99@gmail.com>
2025-02-05 14:13:25 +0100

    modify: show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -14,16 +14,21 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-    const post = posts.find(post => post.id == req.params.id)
-    if (!post) {
-        res.status(404);
-        return res.json({
-            message: 'post non trovato',
-            status: 404,
-            error: 'not found'
-        })
-    }
-    res.json(post);
+    const id = req.params.id;
+    const sqlPost = 'SELECT * FROM posts WHERE id= ?'
+
+    connection.query(sqlPost, [id], (err, results) => {
+        //restituisco errore se non trovo il post
+        if (err) return res.status(500).json({ error: 'Query al database fallita' })
+        if (results.length === 0) return res.status(404).json({ error: 'Post non trovato' });
+
+        //superati i controlli restituisco il primo risultato dell'array
+        let post = results[0];
+
+        res.json(post);
+    })
+
+
 }
 
 const store = (req, res) => {
